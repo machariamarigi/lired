da2406af52acf985a64c2288cbab1465124a71f5
machariamarigi <machariamarigi@gmail.com>
2020-09-23 16:38:58 +0300

    set up main function for the appliction

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World')
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        dbName: 'lired',
+        type: 'postgresql',
+        entities: [Post],
+        debug: __prod__
+    })
+}
+
+
+
+main()
+
