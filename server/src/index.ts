210516cf001c544115dc99110a24ca64cd26e6da
machariamarigi <machariamarigi@gmail.com>
2020-09-25 16:33:54 +0300

    set up main express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,16 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from 'express';
 import MikroORMConfig from './mikro-orm.config'
 
 const main = async () => {
     const orm = await MikroORM.init(MikroORMConfig)
     await orm.getMigrator().up()
+
+    const server = express()
+
+    server.listen(4000, () => {
+        console.log('Server set up on localhost:4000')
+    })
 
 }
 
