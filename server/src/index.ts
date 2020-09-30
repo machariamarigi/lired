6309eaaa818fcced0c79a66a86933740e2ddd977
machariamarigi <machariamarigi@gmail.com>
2020-09-30 16:59:13 +0300

    setup apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
+import "reflect-metadata"
 import { MikroORM } from "@mikro-orm/core";
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
 import MikroORMConfig from './mikro-orm.config'
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(MikroORMConfig)
@@ -8,10 +12,19 @@ const main = async () => {
 
     const server = express()
 
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em })
+    })
+
+    apolloServer.applyMiddleware({ app: server })
+
     server.listen(4000, () => {
         console.log('Server set up on localhost:4000')
     })
-
 }
 
 main().catch(err => console.log(err))
