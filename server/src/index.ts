cf2e1b7789c58be7c1b07508f9772de2afc51ac0
machariamarigi <machariamarigi@gmail.com>
2020-10-09 15:26:41 +0300

    set up redis store and client and use them in the main server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,14 @@ import { MikroORM } from "@mikro-orm/core";
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from "type-graphql";
-import MikroORMConfig from './mikro-orm.config'
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import MikroORMConfig from './mikro-orm.config';
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import { REDIS_SECRET, __prod__ } from "./constants";
+import { MyContext } from "./types";
 
 const main = async () => {
     const orm = await MikroORM.init(MikroORMConfig)
@@ -13,12 +18,29 @@ const main = async () => {
 
     const server = express()
 
+    const RedisStore = connectRedis(session);
+    const redisClient = redis.createClient()
+
+    server.use(
+        session({
+            name: "qid",
+            store: new RedisStore({ client: redisClient }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365, // a year
+                httpOnly: true,
+                sameSite: 'lax',
+                secure: __prod__
+            },
+            secret: REDIS_SECRET
+        })
+    )
+
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [PostResolver, UserResolver],
             validate: false,
         }),
-        context: () => ({ em: orm.em })
+        context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
     })
 
     apolloServer.applyMiddleware({ app: server })
