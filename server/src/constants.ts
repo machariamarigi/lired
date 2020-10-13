ab45fb17b2bdefcba78c72a89ca87dc8808954aa
machariamarigi <machariamarigi@gmail.com>
2020-10-13 13:54:59 +0300

    fix __prod__ bug that was blocking cookies in dev mode

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,2 +1,2 @@
-export const __prod__  = process.env.NODE_ENV !== 'production'
+export const __prod__  = process.env.NODE_ENV === 'production'
 export const REDIS_SECRET = process.env.REDIS_SECRET as string
