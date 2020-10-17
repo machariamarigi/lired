a3f1788c2211e6faae0f5cef489631f205f34912
machariamarigi <machariamarigi@gmail.com>
2020-10-17 19:20:41 +0300

    set cors orgin for localhosr

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,17 +6,24 @@ import { buildSchema } from "type-graphql";
 import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
+import cors from 'cors';
 import MikroORMConfig from './mikro-orm.config';
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 import { REDIS_SECRET, __prod__ } from "./constants";
-import { MyContext } from "./types";
 
 const main = async () => {
     const orm = await MikroORM.init(MikroORMConfig)
     await orm.getMigrator().up()
 
     const server = express()
+
+    server.use(
+        cors({
+            origin: 'http://localhost:3000',
+            credentials: true       
+        })
+    )
 
     const RedisStore = connectRedis(session)
     const redisClient = redis.createClient()
@@ -45,10 +52,13 @@ const main = async () => {
             resolvers: [PostResolver, UserResolver],
             validate: false,
         }),
-        context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+        context: ({ req, res }) => ({ em: orm.em, req, res }),
     })
 
-    apolloServer.applyMiddleware({ app: server })
+    apolloServer.applyMiddleware({
+        app: server,
+        cors: false
+    })
 
     server.listen(4000, () => {
         console.log('Server set up on localhost:4000')
