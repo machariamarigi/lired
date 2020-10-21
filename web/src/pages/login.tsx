a438b920bcb027c6e2bfbae0241d94a595c56e8b
machariamarigi <machariamarigi@gmail.com>
2020-10-21 18:26:32 +0300

    web: add login page with login functionality

---
+++ web/src/pages/login.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Formik, Form } from 'formik'
+import { Box, Button } from '@chakra-ui/core'
+import { useRouter } from 'next/dist/client/router'
+import Wrapper from '../components/Wrapper'
+import InputField from '../components/InputField'
+import { useLoginMutation } from '../generated/graphql'
+import { toErrorMap } from '../utils/toErrorMap'
+
+
+interface LoginProps {
+}
+
+const Login: React.FC<LoginProps> = ({}) => {
+    const [, login] = useLoginMutation()
+    const router = useRouter()
+
+    return ( 
+        <Wrapper varient="Small">
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={ async (values, { setErrors }) => {
+                    const response = await login({ userInput: values })
+                    console.log(response.data?.login)
+                    if (response.data?.login.errors) {
+                        setErrors(toErrorMap(response.data.login.errors))
+                    } else if (response.data?.login.user) {
+                        console.log(response.data?.login.user)
+                        router.push('/')
+                    }
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <InputField name="email" placeholder="Email" label="Email" />
+                        <Box mt={4}></Box>
+                        <InputField name="password" placeholder="Password" label="Password" type="password"/> 
+                        <Button 
+                            mt={4}
+                            type="submit"
+                            colorScheme="green"
+                            isLoading={isSubmitting}
+                        >
+                                Login
+                        </Button>
+                    </Form>
+                    
+                )}
+            </Formik>            
+        </Wrapper>
+
+    )
+}
+
+export default Login
