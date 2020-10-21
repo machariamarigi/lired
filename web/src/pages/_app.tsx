9d9c116c24a8b754cf4d9bf8380febffb492d9b2
machariamarigi <machariamarigi@gmail.com>
2020-10-21 17:17:20 +0300

    add the graphql provider

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import { ChakraProvider } from '@chakra-ui/core'
-
 import theme from '../theme'
 import { AppProps } from 'next/app'
+import { createClient, Provider } from 'urql';
+
+const client = createClient({
+  url: process.env.NEXT_PUBLIC_GRAPHQL_SERVER as string,
+  fetchOptions: {
+    credentials: 'include'
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider value={client}> 
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>      
+    </Provider>
+
   )
 }
 
