4b049c91e80a509b2a33962d175fb2c368e77dba
machariamarigi <machariamarigi@gmail.com>
2020-10-25 00:46:06 +0300

    server: add logout mutation, destroy session and remove cookie

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -3,6 +3,7 @@ import argon2 from "argon2";
 import { EntityManager } from '@mikro-orm/postgresql'
 import { MyContext } from "../types";
 import { User } from "../entities/User";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UserInput {
@@ -129,5 +130,20 @@ export class UserResolver {
         req.session!.userID = user.id
 
         return {user}
+    }
+
+    @Mutation(() => Boolean)
+    logout(
+        @Ctx() {req, res}: MyContext
+    ){
+        return new Promise(resolve => req.session.destroy(err => {
+            res.clearCookie(COOKIE_NAME)
+            if (err) {
+                console.log(err)
+                resolve(false)
+                return
+            }
+            resolve(true)
+        }))
     }
 }
