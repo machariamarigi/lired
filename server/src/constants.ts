15ee45a387c98adfc04282ecd0ca0c16b319f0e5
machariamarigi <machariamarigi@gmail.com>
2020-10-28 14:25:01 +0300

    server: add EMAIL_REGEX as constant

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,3 +1,4 @@
 export const __prod__  = process.env.NODE_ENV === 'production'
 export const REDIS_SECRET = process.env.REDIS_SECRET as string
 export const COOKIE_NAME = "qid"
+export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
