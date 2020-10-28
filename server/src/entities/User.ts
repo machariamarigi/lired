82d14603c4dbea7e6f5c8a5545b07ef08f5cae45
machariamarigi <machariamarigi@gmail.com>
2020-10-28 14:26:48 +0300

    server: add username property to User entity

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -20,6 +20,10 @@ export class User {
   @Property({ type: "text", unique: true })
   email!: string
 
+  @Field()
+  @Property({ type: "text", unique: true })
+  username!: string
+
   @Property({ type: "text" })
   password!: string
 }
