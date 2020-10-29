91012708e8db36b10aa809e6a6949ac12f76b2ff
machariamarigi <machariamarigi@gmail.com>
2020-10-29 16:57:12 +0300

    server: add FORGET_PASSWORD_PREFIX constant to be used on redis

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,3 +2,4 @@ export const __prod__  = process.env.NODE_ENV === 'production'
 export const REDIS_SECRET = process.env.REDIS_SECRET as string
 export const COOKIE_NAME = "qid"
 export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+export const FORGET_PASSWORD_PREFIX = 'forget-password:'
