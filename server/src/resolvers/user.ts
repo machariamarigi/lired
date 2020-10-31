0bccbc399df1e8b1ef0f43e21aef3890cf043dfc
machariamarigi <machariamarigi@gmail.com>
2020-10-31 11:19:02 +0300

    server: delete the change password token after single use

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -200,7 +200,8 @@ export class UserResolver {
             }  
         }
 
-        const userId = await redis.get(FORGET_PASSWORD_PREFIX + token)
+        const redisKey = FORGET_PASSWORD_PREFIX + token
+        const userId = await redis.get(redisKey)
 
         if (!userId) {
             return { 
@@ -228,6 +229,8 @@ export class UserResolver {
 
         user.password = await argon2.hash(newPassword)
         await em.persistAndFlush(user)
+
+        await redis.del(redisKey)
 
         req.session.userId = user.id
 
