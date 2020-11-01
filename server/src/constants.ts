eb6d4df0a0d29e0fa754059c2ec060061d0aeea8
machariamarigi <machariamarigi@gmail.com>
2020-11-01 22:58:31 +0300

    serve: add DATABASE_URL constant set from .env

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,5 +1,6 @@
 export const __prod__  = process.env.NODE_ENV === 'production'
 export const REDIS_SECRET = process.env.REDIS_SECRET as string
+export const DATABASE_URL = process.env.DATABASE_URL
 export const COOKIE_NAME = "qid"
 export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 export const FORGET_PASSWORD_PREFIX = 'forget-password:'
