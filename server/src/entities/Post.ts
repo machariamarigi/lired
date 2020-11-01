d5d4d844f9663e8f752fdd75b9f9c82784cb5f86
machariamarigi <machariamarigi@gmail.com>
2020-11-01 23:38:28 +0300

    server: create one to many relationship between user and posts

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,5 +1,6 @@
 import { Field, ObjectType } from "type-graphql";
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { BaseEntity, Column, CreateDateColumn, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { User } from "./User";
 
 @ObjectType()
 @Entity()
@@ -7,6 +8,17 @@ export class Post extends BaseEntity {
   @Field()
   @PrimaryGeneratedColumn()
   id!: number
+
+  @Field()
+  @Column()
+  title!: string
+
+  @Field()
+  @Column()
+  creatorId: number
+
+  @ManyToOne(() => User, user => user.posts)
+  creator: User
 
   @Field(() => String)
   @CreateDateColumn()
@@ -16,7 +28,4 @@ export class Post extends BaseEntity {
   @UpdateDateColumn()
   updatedAt: Date
 
-  @Field()
-  @Column()
-  title!: string
 }
