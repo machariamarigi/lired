c31e218c681f986570acaffda9073c03eb2af95e
machariamarigi <machariamarigi@gmail.com>
2020-11-01 23:01:13 +0300

    server: replace mikro-orm set up and with typeorm set-up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
 import "reflect-metadata"
-import { MikroORM } from "@mikro-orm/core";
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from "type-graphql";
@@ -7,14 +6,21 @@ import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
-import MikroORMConfig from './mikro-orm.config';
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
-import { COOKIE_NAME, REDIS_SECRET, __prod__ } from "./constants";
+import { COOKIE_NAME, DATABASE_URL, REDIS_SECRET, __prod__ } from "./constants";
+import { createConnection } from "typeorm";
+import { User } from "./entities/User";
+import { Post } from "./entities/Post";
 
 const main = async () => {
-    const orm = await MikroORM.init(MikroORMConfig)
-    await orm.getMigrator().up()
+    const conn = await createConnection({
+        type: "postgres",
+        url: DATABASE_URL,
+        logging: true,
+        entities: [User, Post],
+        synchronize: !__prod__
+    })
 
     const server = express()
 
@@ -52,7 +58,7 @@ const main = async () => {
             resolvers: [PostResolver, UserResolver],
             validate: false,
         }),
-        context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
+        context: ({ req, res }) => ({ req, res, redis }),
     })
 
     apolloServer.applyMiddleware({
