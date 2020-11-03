ef03aa7111669df0c1ded03585aef87bdf2290c0
machariamarigi <machariamarigi@gmail.com>
2020-11-03 21:19:55 +0300

    web: refactor InputField to allow for textareas

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -1,22 +1,28 @@
 import React, { InputHTMLAttributes } from 'react'
-import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/core'
+import { FormControl, FormLabel, Input, FormErrorMessage, Textarea } from '@chakra-ui/core'
 import { useField } from 'formik'
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
     name: string
     label: string
+    textarea?: boolean
 }
 
 const InputField: React.FC<InputFieldProps> = ({
     label,
+    textarea,
     size : _,
     ...props}) => {
+    let InputOrTextarea: any = Input
+    if (textarea) {
+        InputOrTextarea = Textarea
+    }
     const [field, { error }] = useField(props)
 
     return (
         <FormControl isInvalid={!!error}>
             <FormLabel htmlFor={field.name}>{ label }</FormLabel>
-            <Input {...field} {...props} id={field.name} placeholder={props.placeholder} type={props.type} />
+            <InputOrTextarea {...field} {...props} id={field.name} placeholder={props.placeholder} type={props.type} />
             { error ? <FormErrorMessage>{error}</FormErrorMessage> : null  }
         </FormControl>
     );
