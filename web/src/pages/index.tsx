53f54702eec75e67c16fe76710f7d4b12c7cb3f7
machariamarigi <machariamarigi@gmail.com>
2020-11-07 22:50:28 +0300

    web: query 10 posts at a time

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,7 +6,9 @@ import { usePostsQuery } from "../generated/graphql"
 import Layout from "../components/Layout"
 
 const Index = () => {
-    const [{ data }] = usePostsQuery()
+    const [{ data }] = usePostsQuery({
+        variables: { limit: 10 }
+    })
     return (
         <Layout>
             <NextLink href='/create-post'>
