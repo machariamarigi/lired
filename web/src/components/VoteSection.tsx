97fce60b3c7a421765efb60e0ac2ff1ec3aaf18c
machariamarigi <machariamarigi@gmail.com>
2020-11-28 13:54:24 +0300

    web: allow users to upvote and downvote a post

--- web/src/components/VoteSection.tsx
+++ web/src/components/VoteSection.tsx
@@ -1,13 +1,16 @@
+import React, { useState } from 'react'
 import { Flex, IconButton } from '@chakra-ui/core';
 import { ChevronUpIcon, ChevronDownIcon } from '@chakra-ui/icons';
-import React from 'react'
-import { PostSnippetFragment } from '../generated/graphql';
+import { PostSnippetFragment, useVoteMutation } from '../generated/graphql';
 
 interface VoteSectionProps {
     post: PostSnippetFragment
 }
 
 const VoteSection: React.FC<VoteSectionProps> = ({ post }) => {
+    const [, vote] = useVoteMutation()
+    const [loadingState, setLoadingState] = useState<
+        "upvote-loading" | "downvote-loading" | "not-loading">("not-loading")
     return (
         <Flex
             direction="column"
@@ -21,6 +24,15 @@ const VoteSection: React.FC<VoteSectionProps> = ({ post }) => {
                 aria-label="vote +1"
                 size="sm"
                 variant="outline"
+                onClick={async () => {
+                    setLoadingState("upvote-loading")
+                    await vote({
+                        postId: post.id,
+                        value: 1
+                    })
+                    setLoadingState("not-loading")
+                }}
+                isLoading={loadingState==="upvote-loading"}
             />
             { post.points }
             <IconButton
@@ -28,6 +40,15 @@ const VoteSection: React.FC<VoteSectionProps> = ({ post }) => {
                 aria-label="vote -1"
                 size="sm"
                 variant="outline"
+                onClick={async () => {
+                    setLoadingState("downvote-loading")
+                    await vote({
+                        postId: post.id,
+                        value: -1
+                    })
+                    setLoadingState("not-loading")
+                }}
+                isLoading={loadingState==="downvote-loading"}
             />
         </Flex>
     );
