4a535eb42fd93dedaeddb3f88d2fe6b3cdbb1a73
machariamarigi <machariamarigi@gmail.com>
2021-01-01 19:56:41 +0300

    (web) create useGetIntId & useGetPostFromUrl utils to handle getting the post from the url

--- web/src/pages/post/[id].tsx
+++ web/src/pages/post/[id].tsx
@@ -3,20 +3,13 @@ import { useRouter } from 'next/router';
 import { withUrqlClient } from 'next-urql';
 import { Heading } from '@chakra-ui/core';
 import { createUrqlClient } from '../../utils/createUrqlClient';
-import { usePostQuery } from '../../generated/graphql';
 import Layout from '../../components/Layout';
+import { useGetPostFromUrl } from '../../utils/useGetPostFromUrl';
 
 
 
 const Post = ({}) => {
-    const router = useRouter()
-    const intId = typeof router.query.id === 'string' ? parseInt(router.query.id) : -1
-    const [{ data, fetching }] = usePostQuery({
-        pause: intId === -1,
-        variables: {
-            id: intId
-        }
-    })
+    const [{ data, fetching }] = useGetPostFromUrl()
 
     if (fetching) {
         return(
